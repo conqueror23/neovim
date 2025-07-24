d795019cea1a170c4f8a12f6a34f35141c458f2b
bolong.wang@hellofresh.com.au <bolong.wang@hellofresh.com.au>
2025-07-24 11:42:29 +1000

    feat:fix null-ls and aventa

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,16 +1,15 @@
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
+  dependencies = { 
+    "nvim-lua/plenary.nvim",
+    "jay-babu/mason-null-ls.nvim",
+  },
+  opts = function(_, opts)
     local null_ls = require "null-ls"
-
-    null_ls.setup {
-      sources = {
-        -- Use basic eslint sources without custom args for now
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.eslint,
-      },
-    }
+    opts.sources = require("astrocore").list_insert_unique(opts.sources or {}, {
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.prettier,
+    })
   end,
 }
